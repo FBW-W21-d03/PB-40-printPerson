4fb0b341acd759791dffe14ec04d54378418a4e4
sprotte <metatron1972@live.de>
2022-01-04 15:13:23 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 "use strict";
 
 function mensch(name, alter, beruf) {
-  return "ist", alter + "Jahre alt und ist" + beruf.length;
+  return "ist" + alter + "Jahre alt und ist" + beruf.length;
 }
 
-let return1 = mensch("Metatron" + 49 + "Student");
+let return1 = mensch("Metatron", 49, "Student");
 let return2 = mensch("Eli", 32, "Lehrer");
 let return3 = mensch("Harald", 65, "Maurer");
 

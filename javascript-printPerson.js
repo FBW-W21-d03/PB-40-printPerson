bdd334515ba2880e87c5dfc75088f9eaf9775884
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-04 15:09:16 +0100

    Orhan's lösung2

--- javascript-printPerson.js
+++ javascript-printPerson.js
@@ -1,15 +1,10 @@
 // Aufgabe 1
 function person(name, age, beruf) {
 
-    let person = {
-        name: name,
-        alter: age,
-        beruf: beruf
-    };
-
-    console.log(person.name, "ist", person.alter, "Jahre alt und ein", person.beruf, ".");    
+    return name + " ist " + age + " Jahre alt und ein " + beruf + ".";    
 
 }
 
 // Aufgabe 2
-person("Joh", "30", "Web Developer");
+let person1 = person("Joh", 30, "Senger");
+console.log(person1);

d19c0b919373051c80ca4aac89d966e3b7340229
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-04 11:10:53 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,10 @@
+// AUFGABE TEIL-1
+function daten(Name, Alter, Beruf){
+
+    console.log(Name ,"ist" , Alter ,"jahre alt und", Beruf);
+}
+daten("John", "20","Student.");
+
+// AUFGABE TEIL-2
+daten("Mary","25","Doctor.");
+

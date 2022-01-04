2bbda68a5cc5b7009c7db52cd695c4c60e5d07f9
Vens <v.kolev@alfatrex.com>
2022-01-04 11:08:44 +0100

    vens lüsung

---
+++ script.js
@@ -0,0 +1,9 @@
+function person(name, alter, beruf){
+console.log( name, "ist", alter, "Jahre alt und ein" ,beruf );
+}
+
+// Aufgabe 2
+
+person("Ben",19, "Schuller");
+person("John",32,"Jurnalist");
+person("Norman",85,"Rentner")

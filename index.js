67f0a855ed7c62362a3eeee557e8ac7ed9008f92
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-04 11:24:57 +0100

    js

---
+++ index.js
@@ -0,0 +1,9 @@
+function personalInfo (name,old,job){
+    let myObject = {
+       Name :name,
+        Altr:old,
+        Beruf:job,
+    }
+    console.log(myObject);
+}
+personalInfo("John","20","Student")
---
+++ index1.js
@@ -0,0 +1,9 @@
+// 1)
+
+function person(name , age , occupation){
+    console.log(name ,  "ist" , age , "Jahre alt und" ,  occupation + ".")
+}
+// 2)
+person('John' , 20 , "Student");
+person('Sarah' , 31 , 'Student');
+person('Rodi' , 38 , 'Arzt');

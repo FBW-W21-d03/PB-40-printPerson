92a771ba91f1891fe097d78bfffdb34d46d3466c
slavablack <veaceslav.ciornii@magenta.de>
2022-01-04 15:13:34 +0100

    Veaceslav

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function dataBase (name, alter, beruf) {
 };
 dataBase("John", "20 Jahre", "Student");
 dataBase("Clarck", "64 Jahre", "IT Entwickler");
+// Return function 
+function dataBasereturn (name, old, beruf) { 
+
+    return name + " ist " + old + " alt und ein "  + beruf;
+}
+let info = dataBasereturn ("John", "20 Jahre", "Student");
+console.log(info);
+let info2 = dataBasereturn("Clarck", "64 Jahre", "IT Entwickler");
+console.log(info2);

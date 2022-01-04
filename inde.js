1ed5da3cbd5219b7968ab1e91b326c89949dd43f
Serdar68 <Serdarbalkis@outlook.de>
2022-01-04 15:08:31 +0100

    serdar2

--- inde.js
+++ inde.js
@@ -6,13 +6,23 @@ Beispiel: "John ist 20 Jahre alt und ein Student."
 Schreibe dazu eine Funktion, die drei Parameter akzeptiert (Name, Alter, Beruf).
 Die Funktion gibt anschließend den Namen, das Alter und den Beruf in einer Konsolenausgabe aus.*/
 
-function myFunktion(name, alter, beruf){
-console.log(name + " ist " + alter + " Jahre alt und ein " + beruf)
-};
+function myFunktion(name, alter, beruf) {
+  return name + " ist " + alter + " Jahre alt und ein " + beruf;
+}
 
 /* Aufgabenstellung - Teil 2
 Teste deine Funktion.
 Denke dir eine Person mit einem Alter und einem Beruf aus.*/
-myFunktion("Hans", 33, "Tischler");
-myFunktion("Alex", 21, "Profi Fußballer");
-myFunktion("Goethe", 40, "Künstler");
+
+let return1 = myFunktion("Hans", 33, "Tischler");
+let return2 = myFunktion("Alex", 21, "Profi Fußballer");
+let return3 = myFunktion("Goethe", 40, "Künstler");
+
+console.log(return1);
+console.log(return2);
+console.log(return3);
+
+
+
+
+

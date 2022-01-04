102abccdd4e883d5f75261b520fc7cd402e0d5fc
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-01-04 11:46:00 +0100

    Lösung von Christian mit Bonus (Array von Objekten)

--- christian.js
+++ christian.js
@@ -3,3 +3,24 @@ function printPerson(name, age, job) {
 }
 
 printPerson("Peter", 37, "Postbote");
+
+// Bonus: Liste von mehreren Personen
+
+// const personen = [
+//   { name: "Peter", age: 47, job: "Postbote" },
+//   { name: "Angela", age: 37, job: "Bankangestellte" },
+// ];
+
+// console.table(personen);
+
+// Personen sortieren nach Alter
+
+// personen.sort((a, b) => {
+//   return a.age - b.age;
+// });
+
+// Jede Person ausgeben
+
+// personen.forEach((person) => {
+//   printPerson(person.name, person.age, person.job);
+// });

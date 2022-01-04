a7ea109bd5e8d7fb9feb4339482e577aed2d35ff
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-04 15:35:56 +0100

    neu lösung

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
   // Aufgabenstellung - Teil 1
-function person(name, alter, beruf){
-    console.log( name, "ist", alter, "Jahre alt und ein" ,beruf );
-    }
-    
-    // Aufgabenstellung - Teil 2
-    person("Ben",22, "Student");
-    person("John",33,"Anwalt");
-    person("Norman",75,"Rentner")
+  function myFunktion(name, alter, beruf) {
+    return name + " ist " + alter + " Jahre alt und ein " + beruf;
+  }
+
+// Aufgabenstellung - Teil 2
+let return1 = myFunktion("Hans", 38, "Anwalt");
+let return2 = myFunktion("Alex", 18, "Taxifahrer");
+let return3 = myFunktion("Goethe", 40, "Künstler");
+
+console.log(return1);
+console.log(return2);
+console.log(return3);

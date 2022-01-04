ea55b7df5cb02a919251ba7782b6bf7349a16dbc
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-04 11:12:40 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,11 @@
+// Aufgabe 1
+
+function person(name, age, beruf) {
+  console.log(name, "ist", age, "Jahre alt und ein", beruf);
+}
+
+// Aufgabe 2
+
+person("Rajwinder Singh", 30, "Student");
+person("John", 20, "Student");
+person("Smith", 26, "Entwickler");

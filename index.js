063156dad755d8189c33ccfa0ec532e6e37c0a09
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-04 10:28:44 +0100

    printPerson

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+console.log("------------------------");
+console.log("javascript-printPerson");
+
+//aufgabe teil1
+
+function person(name , age , occupation){
+    console.log(name ,  "ist" , age , "Jahre alt und" ,  occupation + ".")
+}
+// aufgabe teil2
+person('Birgitta' , 37 , "Kunstmalerin");
+person('Andreas' , 58 , 'Programmierer');
+person('Olaf' , 66 , 'Musiker');

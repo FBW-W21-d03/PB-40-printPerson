8e6f416d818b89ebc3da974ba9a60fc495ff6743
sham912411 <shamalhakim91@gmail.com>
2022-01-04 11:31:45 +0100

    lösung

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function person(name , age , occupation){
+    console.log(name ,  "ist" , age , "Jahre alt und" ,  occupation + ".")
+}
+
+person('Birgitta' , 37 , "Kunstmalerin");
+person('Andreas' , 58 , 'Programmierer');
+person('Olaf' , 66 , 'Musiker');

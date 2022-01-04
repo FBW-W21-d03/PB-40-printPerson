528f4364d450af7297b6f33710a07e7e1e16c010
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-04 11:19:12 +0100

    Lösung

---
+++ index.js
@@ -0,0 +1,10 @@
+
+  // Aufgabenstellung - Teil 1
+function person(name, alter, beruf){
+    console.log( name, "ist", alter, "Jahre alt und ein" ,beruf );
+    }
+    
+    // Aufgabenstellung - Teil 2
+    person("Ben",22, "Student");
+    person("John",33,"Anwalt");
+    person("Norman",75,"Rentner")

3c46fc4ded0fe50dcdc0da2f0ce63e438469e19e
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-04 15:57:11 +0100

    Die Aufgabe wird mit verwenden von return gelöst

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 // AUFGABE TEIL-1
-function daten(name, age, job){
+// function daten(name, age, job){
 
-    console.log(name ,"ist" , age ,"jahre alt und ein", job);
+//     console.log(name ,"ist" , age ,"jahre alt und ein", job);
+// }
+// daten("John", 20,"Student.");
+
+// // AUFGABE TEIL-2
+// daten("Mary", 25,"Doctor.");
+
+
+
+// AUFGABELÖSUNG MIT VERWENDEN VON RETURN
+function daten(name,age,job){
+ 
+    return  name + " ist " + age + " Jahre alt und ein " + job + ".";
+  
 }
-daten("John", 20,"Student.");
 
-// AUFGABE TEIL-2
-daten("Mary", 25,"Doctor.");
+let person = daten("Melina" , 35 ," pilot");
+console.log(person)
+
+
 

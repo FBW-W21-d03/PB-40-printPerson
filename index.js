d1f9b0c710e7eb69a42fda3bc117d3212b358bfb
sprotte <metatron1972@live.de>
2022-01-04 11:14:32 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,9 @@
+function mensch(name, alter, beruf) {
+  console.log(name, "ist", alter, "Jahre alt und ein", beruf);
+}
+
+// Aufgabe 2
+
+mensch("Metatron", 49, "Student");
+mensch("Eli", 32, "Lehrer");
+mensch("Harald", 65, "Maurer");

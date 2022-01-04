c1ed2038a7d3a212f7b0fc2f86a8c3d810176b8a
kawanaza <kawanbabani@gmail.com>
2022-01-04 11:31:49 +0100

    done

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
-function profile ( name,alt,beruf){
-    let personProfie={
-    Name:name,
-    alt:alt,
-    Beruf:beruf
+/// Lösung mit object ///
+
+function profile(name, alt, beruf) {
+  let personProfie = {
+    Name: name,
+    alt: alt,
+    Beruf: beruf,
+  };
+  console.table(personProfie);
 }
-console.table(personProfie);
-};
+
+profile("John", 20, "Student");
 
 
-profile("John",20,"Student")
+
+/// Zweite Lösung als function parameter ////
+function person(name, alter, beruf) {
+  console.log(name, "ist", alter, "Jahre alt und ein", beruf);
+}
+
+
+
+person("kawan", 34, "Web Developer");
+

8f4db47b4cbe2263c851584772adb16b416e4686
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-04 15:41:47 +0100

    js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // 1)
 
 function person(name , age , job){
-    console.log(name ,  "ist" , age , "Jahre alt und" ,  job + ".")
+    return name +  "ist" + age + "Jahre alt und" +  job + ".";
+
 }
 // 2)
-person('John' , 20 , "Student");
-person('Sarah' , 31 , 'Student');
-person('Rodi' , 38 , 'Arzt');
+let return1 = person('John' , 20 , "Student"); 
+let return2 = person('Sarah' , 31 , 'Student');
+let return3 = person('Rodi' , 38 , 'Arzt');
+console.log(return1);
+console.log(return2);
+console.log(return3);

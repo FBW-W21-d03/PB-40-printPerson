c2020dd4cfcd5c02b65f22ea758fd40044685339
slavablack <veaceslav.ciornii@magenta.de>
2022-01-04 11:10:44 +0100

    Veaceslav

---
+++ index.js
@@ -0,0 +1,5 @@
+function dataBase (name, alter, beruf) {
+    console.log(name, "ist " + alter, "alt und ein "  + beruf);
+};
+dataBase("John", "20 Jahre", "Student");
+dataBase("Clarck", "64 Jahre", "IT Entwickler");
